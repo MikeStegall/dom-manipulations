466f2848a8bb48b9549344e1acd4a0ff330a30e7
MikeStegall <mestegall5@gmail.com>
2017-04-01 09:28:43 -0500

    finished the first 2 tasks

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 
-document.querySelector("#thanks button").addEventListener('click',function(){
+document.querySelector('#thanks button').addEventListener('click', function () {
   // TASK #1
+  alert("yowch! don't click me so hard!")
 })
 
-document.querySelector("#double button").addEventListener('click',function(){
+document.querySelector('#double button').addEventListener('click', function () {
   // TASK #2
+  var double = document.querySelector('#compoundInvestment').innerHTML
+  document.querySelector('#compoundInvestment').innerHTML = double * 2
 })
 
-
-document.querySelector("#color-circle button").addEventListener('click',function(){
-  // TASK #3
-})
-
-document.querySelector("#blow-up button").addEventListener('click',function(){
-  // TASK #4}
-})
-
-document.querySelector("#remove button").addEventListener('click',function(){
-  // TASK #5
-})
-
-
-document.querySelector("#reverse-squares button").addEventListener('click',function(){
-  // TASK #6
-})
-
-document.querySelector("#pig-latin button").addEventListener('click',function(){
-  // TASK #7
-})
-
-document.querySelector("#cycle-image button").addEventListener('click',function(){
-  // TASK #8
-})
+// document.querySelector('#color-circle button').addEventListener('click', function () {
+//   // TASK #3
+// })
+//
+// document.querySelector('#blow-up button').addEventListener('click', function () {
+//   // TASK #4}
+// })
+//
+// document.querySelector('#remove button').addEventListener('click', function () {
+//   // TASK #5
+// })
+//
+// document.querySelector('#reverse-squares button').addEventListener('click', function () {
+//   // TASK #6
+// })
+//
+// document.querySelector('#pig-latin button').addEventListener('click', function () {
+//   // TASK #7
+// })
+//
+// document.querySelector('#cycle-image button').addEventListener('click', function () {
+//   // TASK #8
+// })
